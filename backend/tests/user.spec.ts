c753a9f23b8cf7fd536258a8d46d9664c9ca398c
Gabriel Luders <ludersdev@gmail.com>
2021-09-22 21:57:10 -0300

    feat: finished tests for user

--- backend/tests/user.spec.ts
+++ backend/tests/user.spec.ts
@@ -1,10 +1,10 @@
 import { User, UserRegisterInput } from '../src/components/user/user.dto'
 import { userService } from '../src/components/user/user.service'
 
-describe('testing user service', () => {
+describe('user service', () => {
   const service = userService
 
-  test('testing register function', async () => {
+  test('should register a user', async () => {
     const userData: UserRegisterInput = {
       email: 'test@test.com',
       name: 'test seed',
@@ -21,5 +21,33 @@ describe('testing user service', () => {
       const castedKey = key as keyof UserRegisterInput
       expect(dbUser[key as keyof User]).toBe(userData[castedKey])
     }
+  })
+
+  test('should delete a user', async () => {
+    const userData = {
+      id: '6f5bd7a2-fa9a-4ea4-a9cf-4bd22e7b6568'
+    }
+
+    const user = await service.delete(userData.id)
+    expect(user).not.toBeNull()
+
+    const checkUser = await service.find(userData.id)
+    expect(checkUser).toBeNull()
+  })
+
+  test('should find a user', async () => {
+    const userData = {
+      id: '6f5bd7a2-fa9a-4ea4-a9cf-4bd22e7b6569'
+    }
+
+    const user = await service.find(userData.id)
+    expect(user).not.toBeNull()
+    expect(user?.id).toBe(userData.id)
+  })
+
+  test('should find all users', async () => {
+    const users = await service.findAll()
+    expect(users).toBeInstanceOf(Array)
+    expect(users).toHaveLength(5)
   })
 })
